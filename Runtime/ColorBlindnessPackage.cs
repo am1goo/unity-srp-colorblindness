21f9d549b180d1c002def419930d3d69c508f30b
p.shestakov <0xShestakov@gmail.com>
2025-05-26 12:14:21 +0300

    correct package path inside of ColorBlindnessPackage

--- Runtime/ColorBlindnessPackage.cs
+++ Runtime/ColorBlindnessPackage.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class ColorBlindnessPackage : MonoBehaviour
 {
-    public static readonly string packagePath = "Packages/com.am1goo.unity-srp-colorblindness";
+    public static readonly string packageName = "com.am1goo.unity.srp.colorblindness";
+    public static readonly string packagePath = $"Packages/{packageName}";
 }
